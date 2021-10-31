37060b68599754977dbe50910052c88d7514417f
bkjones <bkjones@andrew.cmu.edu>
2021-10-31 12:25:25 -0700

    laying out file structure for simulation

---
+++ main.py
@@ -0,0 +1,17 @@
+
+import json
+from arg_parser import *
+
+
+if __name__ == "__main__":
+
+    args = simulation_args()
+
+    # import simulation parameter file
+
+    # import recorded flight data
+
+    # initialize QuadRotor class
+
+    # loop through control inputs (presumably motor speed commands?)
+
