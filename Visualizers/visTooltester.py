8079800b6f81e082dfbdef559f7e174e9f7cddc0
zvicklan <zvicklan@andrew.cmu.edu>
2021-11-09 23:58:33 -0500

    Updated gitignore to ignore image files. Standardized directory usage

--- Visualizers/visTooltester.py
+++ Visualizers/visTooltester.py
@@ -10,7 +10,7 @@ from visTool import visualize
 #Set up variables
 sys.path.append("C:/Users/zvick/OneDrive/Documents/GitHub/dynamics_model_learning/dataLoader")
 from dataLoader.DataLoader import DataLoader
-DL = DataLoader("C:/Users/zvick/OneDrive/Documents/GitHub/dynamics_model_learning/dataset/processed_data")
+DL = DataLoader("C:/Users/zvick/OneDrive/Documents/GitHub/dynamics_model_learning/processed_data")
 
 DL.load_selected_data('2021-02-05-14-00-56')#"2021-02-03-16-10-37")
 timeVec = DL.get_time_values()
