8729d819549a6f5ceae781c17f32f532cc0eb6d3
zvicklan <zvicklan@andrew.cmu.edu>
2021-11-11 01:16:29 -0500

    Implemented whitebox. Not good results

---
+++ whitebox/helpers.py
@@ -0,0 +1,293 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+def getKt(stateMatrix, accz, mass):
+    #Calculate kt using a linear regression
+    
+    #accz = a*kt + b
+    #regression will be using a, accz-b
+    a, b = getABlooper(stateMatrix, mass)
+    otherSide = accz.reshape((-1,1)) - b #column vecs
+    
+    kt, _, _, _ = np.linalg.lstsq(a, otherSide, rcond=-1)
+
+    return kt[0,0] #the coefficient
+    
+def getABlooper(stateMatrix, mass):
+    #Run through all sets and find the a and b values
+
+    numRows = stateMatrix.shape[0]
+
+    #stash values for each row in the state
+    aVec = np.zeros((numRows, 1))
+    bVec = np.zeros((numRows, 1))
+    
+    for ii in range(0, numRows):
+        thisState = stateMatrix[ii, :]
+        a,b = getAB(thisState, mass)
+        aVec[ii] = a
+        bVec[ii] = b
+
+    #and return
+    return aVec, bVec
+
+def getAB(stateVec, mass):
+    #Do the actual calculations
+    
+    #Extract the inputs
+    pos   = stateVec[0:3]
+    vel   = stateVec[3:6]
+    quat  = stateVec[6:10]
+    omega = stateVec[10:13]
+    motor = stateVec[13:17]
+
+    #Prep a
+    #a = sum(motors)/m
+    a = sum(motor) / mass
+
+    #Prep the b terms
+    #b = Q^T*[0;0;-g][2] - (w x v)[2]
+    L = Lmat(quat)
+    R = Rmat(quat)
+    H = Hmat()
+    
+    Q = H.transpose() @ L @ R.transpose() @ H
+    
+    gVec = np.zeros((3,1))
+    gVec[2] = -9.81
+    
+    crossTerm = np.cross(omega, vel)
+    bVec = Q.transpose() @ gVec - crossTerm.reshape((3,1))
+    b = bVec[2] #only want the z component
+    
+    return a,b
+
+def getJxx(stateMatrix, angAcc, kt, sLen):
+    #Performs a regression to get Jxx value for inertia matrix
+    # assumes Jxx = Jyy = 1/2 Jzz
+    
+    a, b = getABlooperJxx(stateMatrix, angAcc, kt, sLen)
+
+    #solving for a*J_11 = b
+    Jxx, _, _, _ = np.linalg.lstsq(a, b, rcond=-1)
+    
+    return Jxx[0,0]
+
+def getABlooperJxx(stateMatrix, angAcc, kt, sLen):
+    #Run through all sets and find the a and b values
+
+    numRows = stateMatrix.shape[0]
+
+    #stash values for each row in the state
+    aVec = np.zeros((numRows*2, 1))
+    bVec = np.zeros((numRows*2, 1))
+    
+    for ii in range(0, numRows):
+        thisState = stateMatrix[ii, :]
+        angAccVec = angAcc[ii, :]
+        
+        a1,b1, a2,b2 = getABJxx(thisState, angAccVec, kt, sLen)
+        aVec[ii*2] = a1
+        bVec[ii*2] = b1
+        aVec[ii*2+1] = a2
+        bVec[ii*2+1] = b2
+
+    #and return
+    return aVec, bVec
+
+def getABJxx(stateVec, angAccVec, kt, sLen):
+    #Do the actual calculations
+    
+    #Extract the inputs
+    pos   = stateVec[0:3]
+    vel   = stateVec[3:6]
+    quat  = stateVec[6:10]
+    omega = stateVec[10:13]
+    motor = stateVec[13:17]
+
+    angAccX = angAccVec[0]
+    angAccY = angAccVec[1]
+    #Prep a
+    #Comes from lines
+    #w\dot_1 = 1/J_11*s*kt*(u2-u4) - w_2w_3
+    #(w\dot_1 + w_2w_3)J_11 = s*kt*(u2-u4)
+    # ------  a  -----        ---  b ----
+    #w\dot_2 = 1/J_22*s*kt*(u3-u1) + w_1w_3
+    #
+    #(w\dot_2 + w_1w_3)J_22 = s*kt*(u3-u1)
+    # ------  a  -----        ---  b ----
+    #
+    #Note: We're multiplying the J term across to make the regression easier
+    # more reliable than regressing for the inverse of our variable... ew
+    a1 = angAccX + omega[1]*omega[2]
+    a2 = angAccY - omega[0]*omega[2]
+
+    #Prep the b terms
+    #b = s*kt*(u2-u4)
+    #b = s*kt*(u3-u1)
+    
+    b1 = sLen * kt * (motor[1] - motor[3])
+    b2 = sLen * kt * (motor[2] - motor[0])
+    
+    return a1,b1, a2,b2
+
+def getKm(stateMatrix, angAcc, Jzz):
+    #Performs a regression to get Jxx value for inertia matrix
+    # assumes Jxx = Jyy = 1/2 Jzz
+    
+    a, b = getABlooperJxx(stateMatrix, angAcc, Jzz)
+
+    #solving for a*J_11 = b
+    km, _, _, _ = np.linalg.lstsq(a, b, rcond=-1)
+    
+    return km[0,0]
+
+def getABlooperKm(stateMatrix, angAcc, Jzz):
+    #Run through all sets and find the a and b values
+
+    numRows = stateMatrix.shape[0]
+
+    #stash values for each row in the state
+    aVec = np.zeros((numRows, 1))
+    bVec = np.zeros((numRows, 1))
+    
+    for ii in range(0, numRows):
+        thisState = stateMatrix[ii, :]
+        angAccVec = angAcc[ii, :]
+        
+        a,b = getABkm(thisState, angAccVec, Jzz)
+        aVec[ii] = a
+        bVec[ii] = b
+
+    #and return
+    return aVec, bVec
+
+def getABkm(stateVec, angAccVec, Jzz):
+    #Do the actual calculations
+    
+    #Extract the inputs
+    pos   = stateVec[0:3]
+    vel   = stateVec[3:6]
+    quat  = stateVec[6:10]
+    omega = stateVec[10:13]
+    motor = stateVec[13:17]
+
+    angAccZ = angAccVec[2]
+    #based on equation
+    # w\dot_3 = 1/J_33*(u1-u2+u3-u4)*km
+    # -- b --   -------   a -------
+    
+    #Prep a
+    a = 1/Jzz * (motor[0] - motor[1] + motor[2] - motor[3])
+
+    #Prep the b terms
+    #b = angAccZ
+    b = angAccZ
+    
+    return a,b
+
+
+def NewtEulPredict(state, mass, sLen, kt, km, J):
+    #Do the prediction for all input states
+    numStates  = state.shape[0]
+    accPred    = np.zeros((numStates, 3))
+    angAccPred = np.zeros((numStates, 3))
+
+    for ii in range(0, numStates):
+        newAcc, newAngAcc = doNewtEul(state[ii, :], mass, sLen, kt, km, J)
+
+        #reshape to avoid issues and plug 'em in!
+        newAcc    = newAcc.reshape((1,3))
+        newAngAcc = newAngAcc.reshape((1,3))
+
+        accPred[ii,:]    = newAcc
+        angAccPred[ii,:] = newAngAcc
+    
+    return accPred, angAccPred
+
+def doNewtEul(stateVec, mass, sLen, kt, km, J):
+    #Do the actual Newton Euler prediction for a particular state
+    # v\dot = 1/m*F-wxv
+    # w\dot = inv(J)*(tau - cross(w, Jw))
+    
+    #Extract the inputs
+    pos   = stateVec[0:3]
+    vel   = stateVec[3:6]
+    quat  = stateVec[6:10]
+    omega = stateVec[10:13]
+    motor = stateVec[13:17]
+
+    L = Lmat(quat)
+    R = Rmat(quat)
+    H = Hmat()
+    
+    #First do acc
+    F = np.zeros((3,1))
+    F[2] = kt*sum(motor)
+    #don't forget gravity
+    g = 9.81;
+    gVec = np.zeros((3,1))
+    gVec[2] = -g
+    
+    Q = H.transpose() @ L @ R.transpose() @ H
+    
+    Fterm = F + Q.transpose() @ gVec
+    crossTerm = np.cross(omega.reshape((1,3)), vel.reshape((1,3)))
+
+    acc = 1/mass * Fterm - crossTerm.reshape((3,1))
+    
+    #And angular acc
+    tauMat = np.zeros((3,4))
+    tauMat[0,1] = sLen * kt
+    tauMat[0,3] = -sLen * kt
+    tauMat[1,0] = -sLen * kt
+    tauMat[1,2] = sLen * kt
+    tauMat[2,0] = km
+    tauMat[2,1] = -km
+    tauMat[2,2] = km
+    tauMat[2,3] = -km
+    tauTerm = tauMat @ motor.reshape((4,1))
+
+    Jw = J @ omega.reshape((3,1))
+    crossTerm = np.cross(omega.reshape((1,3)), Jw.reshape((1,3)))
+    angAcc = np.linalg.inv(J) @ (tauTerm - crossTerm.reshape((3,1)))
+
+    return acc, angAcc
+def hat(x):
+    return np.array([[0, -x[2], x[1]], [x[2], 0, -x[0]], [-x[1], x[0], 0]])
+
+def Lmat(q):
+    q_vec_hat = hat(q[1:])
+    
+    L = np.zeros((4,4))
+    L[0,0] = q[0]
+    L[0,1:] = -q[1:]
+    L[1:,0] = q[1:]
+    L[1:,1:] = q[0]*np.eye(3) + q_vec_hat
+    return L
+
+def Rmat(q):
+    q_vec_hat = hat(q[1:])
+    
+    R = np.zeros((4,4))
+    R[0,0] = q[0]
+    R[0,1:] = -q[1:]
+    R[1:,0] = q[1:]
+    R[1:,1:] = q[0]*np.eye(3) - q_vec_hat
+    return R
+
+def Hmat():
+    #not sure this needs to be a function, but it's handy
+    H = np.zeros((4,3))
+    H[1,0] = 1
+    H[2,1] = 1
+    H[3,2] = 1
+    return H
+
+def quat_dot(q, w):
+    q_vec_hat = hat(q[1:])
+
+    L = Lmat(q)
+
+    # np.vstack term is H from the notes [0; I(3)]
+    return 0.5 * L @ np.vstack([np.zeros(3), np.eye(3)]) @ w
---
+++ whitebox/testing.py
@@ -0,0 +1,60 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import sys
+#Set up environment
+sys.path.append('.')
+sys.path.append('..')
+sys.path.append('../Visualizers')
+sys.path.append('../whitebox')
+from visTool import *
+from helpers import *
+
+#Set up variables
+sys.path.append("../dataLoader")
+from dataLoader.DataLoader import DataLoader
+DL = DataLoader('../processed_data')
+
+print('Loading data')
+DL.load_selected_data('2021-02-05-14-00-56')#"2021-02-03-16-10-37")
+timeVec = DL.get_time_values()
+state   = DL.get_state_data()
+accx = DL.data['acc x'].values
+accy = DL.data['acc y'].values
+accz = DL.data['acc z'].values
+acc = np.concatenate((accx.reshape((-1,1)),
+                      accy.reshape((-1,1)),
+                      accz.reshape((-1,1))), axis=1)
+
+mass = 0.752 #kg (from paper)
+sLen = 0.126 #m half motor-motor len (https://armattanquads.com/chameleon-ti-6-inch/)
+
+print('Calculating kt (prop thrust coefficient)')
+kt = getKt(state, accz, mass)
+print('Using kt value ' + str(round(kt, 5)))
+
+#Calculate the J, km values
+angaccx = DL.data['ang acc x'].values
+angaccy = DL.data['ang acc y'].values
+angaccz = DL.data['ang acc z'].values
+angAcc = np.concatenate((angaccx.reshape((-1,1)),
+                         angaccy.reshape((-1,1)),
+                         angaccz.reshape((-1,1))), axis=1)
+print('Calculating inertia matrix')
+Jxx = getJxx(state, angAcc, kt, sLen)
+print('Using Jxx = ' + str(jxx))
+Jyy = Jxx
+Jzz = 2*Jxx
+J = np.diag([Jxx, Jyy, Jzz])
+
+#Now we need the km value
+print('Calculating prop moment coefficient')
+km = getKm(state, angAcc, Jzz)
+print('Using km = ' + str(km))
+
+#Now we can repredict the data and see what our error looks like
+accPred, angAccPred = NewtEulPredict(state, mass, sLen, kt, km, J)
+
+#Last, to plot (we probably also want some sort of a metric
+totalAcc = np.concatenate((acc, angAcc), axis=1)
+totalNE = np.concatenate((accPred, angAccPred), axis=1)
+
