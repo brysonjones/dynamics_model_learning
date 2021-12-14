fixing bug in moving average filter - thanks Zach!

dataLoader/DataLoader.py
@@ -142,9 +142,9 @@ class DataLoader(object):
                 new_data = np.insert(new_data, i, old_datum)
 
             #adding estimates for last few datapoints
-            for i in range(self.N//2):
+            for i in range(self.N//2, 0, -1):
                 old_datum = np.sum(data[col].values[-i-1:]) / (i+1)
-                new_data = np.insert(new_data, -1, old_datum)
+                new_data = np.append(new_data, old_datum)
 
             if plot_filtering:
                 t_vals = data['t'].values

model/compare.py
@@ -43,8 +43,8 @@ def compare_(args, model, wandb=False):
     col_names = pd.read_csv("processed_data/merged_2021-02-03-13-43-38_seg_1.csv").keys().values
     print(col_names)
     # DL.load_selected_data('2021-02-05-14-00-56', cols_to_filter=col_names[1:4]) # from train set
-    DL.load_selected_data('2021-02-03-17-14-47', cols_to_filter=col_names[1:4])
-    # DL.load_selected_data("2021-02-23-22-54-17", cols_to_filter=col_names[1:4]) # from val set
+    # DL.load_selected_data('2021-02-03-17-14-47', cols_to_filter=col_names[1:4])
+    DL.load_selected_data("2021-02-23-22-54-17", cols_to_filter=col_names[1:4]) # from val set
     DL.saveData('compare_dataset.npz')
     comp_dataset = np.load('compare_dataset.npz')
     comp_inputs = comp_dataset["input"]